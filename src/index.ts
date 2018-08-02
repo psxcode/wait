17ff8e4bee5450872bf68f36795e8c43fdfccbe2
alex.feinstein <alex.feinstein@klarna.com>
2018-08-02 09:35:37 +0200

    add index

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as wait } from './wait'
+export { default as waitPromise } from './wait-promise'
+export { default as waitTime } from './wait-time'
+export { default as waitTimePromise } from './wait-time-promise'
+export { default as ping } from './ping'
